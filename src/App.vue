30f6b3127474c6631e0dd60cde736ceb89282a9e
Christophe <christophe.alexandre@pm.me>
2024-10-17 12:45:51 +0200

    dev

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" v-if="isAuthenticated">
+    <HelloWorld msg="Welcome to Your Vue.js App" v-if="isAuthenticated"/>
+    <HankoAuth v-if="!isAuthenticated" @onSessionCreated="onAuthenticated" />
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HankoAuth from './components/HankoAuth.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HankoAuth
+  },
+  data() {
+    return {
+      isAuthenticated: false  // Initial state: user is not authenticated
+    }
+  },
+  methods: {
+    onAuthenticated() {
+      this.isAuthenticated = true;
+    }
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,29 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
+import { defineConfig } from '@vue/cli-service';
+import { fs } from 'fs';
+import { path } from 'path';
+
+export default defineConfig({
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => {
+        compilerOptions = {
+          isCustomElement: tag => tag.startsWith('hanko-')
+        }
+        return options
+      })
+  },
+  devServer: {
+    host: 'lex-bot.streamlit.app',
+    port: 8443,
+    server: {
+      type: 'https',
+      options: {
+        key: fs.readFileSync(path.resolve(__dirname, '.certs/localhost+3-key.pem')),
+        cert: fs.readFileSync(path.resolve(__dirname, '.certs/localhost+3.pem')),
+      }
+    }
+  }
 })
