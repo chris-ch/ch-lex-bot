1165c50d27ed3dd688c73a0164102ec524dbe06d
Christophe <christophe.alexandre@pm.me>
2025-04-24 14:13:24 +0200

    dev

--- vite.config.ts
+++ vite.config.ts
@@ -6,29 +6,38 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import fs from 'fs'
 import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: tag => tag.startsWith('hanko-'),
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve' // 'serve' = vite dev, 'build' = vite build
+
+  return {
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: tag => tag.startsWith('hanko-'),
+          },
         },
+      }),
+      vueJsx(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+
+    // ⬇️ only include HTTPS + custom host/port while running `vite dev`
+    ...(isDev && {
+      server: {
+        https: {
+          key: fs.readFileSync(path.resolve(__dirname, '.certs/localhost.key')),
+          cert: fs.readFileSync(
+            path.resolve(__dirname, '.certs/localhost.crt'),
+          ),
+        },
+        host: process.env.VITE_HOST || 'localhost',
+        port: Number(process.env.VITE_PORT) || 8443,
       },
     }),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, '.certs/localhost.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, '.certs/localhost.crt')),
-    },
-    host: process.env.VITE_HOST || 'localhost',
-    port: Number(process.env.VITE_PORT) || 8443,
-  },
+  }
 })
