442c63417e802ce833738bbeeed144af1f6faf05
Christophe <christophe.alexandre@pm.me>
2025-05-09 13:49:35 +0200

    feat: add watchEffect import to ProfileDropdown component

--- src/components/ProfileDropdown.vue
+++ src/components/ProfileDropdown.vue
@@ -18,7 +18,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watchEffect } from 'vue'
 import { useUserStore } from '@/stores/userStore'
 import { useI18n } from 'vue-i18n'
 
